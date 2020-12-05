d23e7179a82d0e808c9939b1f4576d172b142f9b
Unknown <bryan.santelices@usach.cl>
2020-12-04 22:43:42 -0300

    Parte 1 depurada y parte 2 lista

--- lab2_parte1.m
+++ lab2_parte1.m
@@ -8,9 +8,9 @@ H1 = (8*s)/(6*s + 2);
 % Lazo abierto 
 figure
 plot(step(H1));
-title('H1 Abierto');
-xlabel('t');
-xlabel('A');
+title('H1 Lazo Abierto');
+xlabel('Tiempo(segundos)');
+ylabel('Amplitud');
 grid on
 
 
@@ -18,9 +18,9 @@ grid on
 H1_cerrado = feedback(H1,-1);
 figure
 plot(step(H1_cerrado));
-title('H1 Cerrado');
-xlabel('t');
-xlabel('A');
+title('H1 Lazo Cerrado');
+xlabel('Tiempo(segundos)');
+ylabel('Amplitud');
 grid on
 
 
@@ -30,18 +30,18 @@ H2 = (8*(s^2) + 7*s + 4)/((s^2) + 6*s + 3);
 % Lazo abierto 
 figure
 plot(step(H2));
-title('H2 Abierto');
-xlabel('t');
-xlabel('A');
+title('H2 Lazo Abierto');
+xlabel('Tiempo(segundos)');
+ylabel('Amplitud');
 grid on
 
 % Lazo cerrado
 H2_cerrado = feedback(H2,-1);
 figure
 plot(step(H2_cerrado));
-title('H2 Cerrado');
-xlabel('t');
-xlabel('A');
+title('H2 Lazo Cerrado');
+xlabel('Tiempo(segundos)');
+ylabel('Amplitud');
 grid on
 
 
---
+++ lab2_parte2.m
@@ -0,0 +1,19 @@
+s = tf ('s');
+Funcion1 = (8*s)/(4*s + 2);
+Funcion2 = (6)/(s + 5);
+Funcion3 = (4*s + 3)/(3*(s^3) + 7*(s^2) + 1);
+Funcion4 = (6)/(7*s + 1);
+Funcion5 = (5*s + 3)/(5*(s^3) + (s^2) + 4);
+Funcion6 = (5*s + 1)/((s^3) + 6*(s^2) + 5);
+
+F3Cerrado = feedback(Funcion3,+1);
+
+Salida = Funcion1 + Funcion2 + (Funcion6 * ((Funcion4 * F3Cerrado) + (Funcion5 * F3Cerrado)))
+
+figure
+plot(step(Salida));
+title('Grafico de respuesta al escalon');
+xlabel('Tiempo(segundos)');
+ylabel('Amplitud');
+grid on
+
