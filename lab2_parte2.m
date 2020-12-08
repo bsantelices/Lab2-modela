9c0e34aa298360fa33a4504805f341f636bd471b
Unknown <bryan.santelices@usach.cl>
2020-12-08 15:16:10 -0300

    Solo falta analizar lo de la ganancia
    arregle todo lo de los gráficos y los feedbacks

--- lab2_parte2.m
+++ lab2_parte2.m
@@ -8,16 +8,17 @@ Funcion5 = (5*s + 3)/(5*(s^3) + (s^2) + 4);
 Funcion6 = (5*s + 1)/((s^3) + 6*(s^2) + 5);
 
 %Retroalimentacion de funcion3
-F3Cerrado = feedback(Funcion3,+1);
+F3Cerrado = feedback(Funcion3,1 ,+1);
 
 %Respuesta del diagrama
 Salida = Funcion1 + Funcion2 + (Funcion6 * ((Funcion4 * F3Cerrado) + (Funcion5 * F3Cerrado)))
 
 %Grafico
 figure
-plot(step(Salida));
+step(Salida);
 title('Grafico de respuesta al escalon');
 xlabel('Tiempo(segundos)');
 ylabel('Amplitud');
 grid on
+saveas(gcf,'Grafico de respuesta al escalon.png')
 
